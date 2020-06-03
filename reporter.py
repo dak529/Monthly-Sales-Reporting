3999650cc7df9a95f06c582a2523e8548a65fbfa
dak529 <dak529@stern.nyu.edu>
2020-06-02 21:09:39 -0400

    Part 1 Exercise

--- reporter.py
+++ reporter.py
@@ -1,5 +1,5 @@
 # reporter.py
-
+import pandas as pd
 def to_usd(my_price):
     """
     Converts a numeric value to usd-formatted string, for printing and display purposes.
@@ -12,4 +12,37 @@ def to_usd(my_price):
     """
     return f"${my_price:,.2f}" #> $12,000.71
 
-print("GENERATING SALES REPORT FOR MONTH OF OCTOBER 2013...")
+print("GENERATING SALES REPORT FOR MONTH[] OF OCTOBER 2014...")
+
+url = 'https://raw.githubusercontent.com/prof-rossetti/intro-to-python/master/data/monthly-sales/sales-201904.csv'
+df = pd.read_csv(url, error_bad_lines=False)
+#print(df)
+
+revenue = df["sales price"].sum()
+print(revenue)
+
+#df["month"] = pd.datetimeindex
+
+df['year'] = pd.DatetimeIndex(df["date"]).year
+df.head()
+
+#print(df)
+
+#df['year'] = pd.DatetimeIndex(df[columns]).year
+#df.head()
+
+df['month'] = pd.DatetimeIndex(df["date"]).month
+df.head()
+
+df['monthname'] = pd.to_datetime(df['month'], format='%m').dt.month_name()
+
+#print(df)
+
+month = df.at[0,"monthname"]
+#print(month)
+year = df.at[0,"year"]
+#print(year)
+
+print("Sales Report",month, year)
+
+print("Total Sales:", to_usd(float(revenue)))
